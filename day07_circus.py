33e9c19a5b16b8d4f477f4f010ae0c1b8ebb810b
Francisco G Silva <giordanofrancisco@hotmail.com>
2019-11-26 20:28:08 +0000

    day07_part1 part2 checked solution

--- day07_circus.py
+++ day07_circus.py
@@ -2,17 +2,17 @@
 https://adventofcode.com/2017/day/7
 """
 import re
-from typing import NamedTuple, List, Union, Pattern, Generic, Set  
-from collections import namedtuple
+from typing import NamedTuple, List, Optional, Pattern, Iterator, Set  
+from collections import Counter, defaultdict
 
 aboves = List[str]
 
 patt = re.compile("(?P<name>[a-z]+)\s\((?P<weight>[0-9]+)\)")
 
-class program(NamedTuple):
+class Program(NamedTuple):
     name: str
     weight: int
-    aboves: Union[aboves, str] 
+    aboves: Optional[aboves] 
 
 def process_line(line:str, pattern: Pattern) -> NamedTuple:
     """
@@ -21,26 +21,27 @@ def process_line(line:str, pattern: Pattern) -> NamedTuple:
     """
     parts = line.split("->")
     if len(parts) == 1:
-        aboves = ""
+        aboves = []
     else:
         aboves = [name.strip() for name in parts[1].split(',')]
 
     g = pattern.search(parts[0])
 
-    return program(name=g.group('name'), 
+    return Program(name=g.group('name'), 
                    weight=int(g.group('weight')), 
                    aboves = aboves)
 
-assert process_line(line="mmqyju (156) -> rjzvwv, noybkx", pattern=patt) ==  program(name='mmqyju', weight=156, aboves=['rjzvwv', 'noybkx'])
-assert process_line(line="mmqyju (156)", pattern=patt) ==  program(name='mmqyju', weight=156, aboves="")
+assert process_line(line="mmqyju (156) -> rjzvwv, noybkx", pattern=patt) ==  Program(name='mmqyju', weight=156, aboves=['rjzvwv', 'noybkx'])
+assert process_line(line="mmqyju (156)", pattern=patt) ==  Program(name='mmqyju', weight=156, aboves=[])
 
-def process_lines(lines:str, pattern: Pattern) -> List[NamedTuple]:
-    return (process_line(program, patt) for program in lines.split("\n"))
+def process_lines(lines:str, pattern: Pattern) -> Iterator[List[Program]]:
+    for program in lines.split("\n"):
+        yield process_line(program, patt)
 
-def is_below(lines:str, pattern: Pattern) -> Set[NamedTuple]:
+def is_below(lines:str, pattern: Pattern) -> Set[str]:
     return set(program.name for program in process_lines(lines= lines, pattern = patt))
 
-def is_above(lines:str, pattern: Pattern) -> Set[NamedTuple]:
+def is_above(lines:str, pattern: Pattern) -> Set[str]:
     return set(pgr for program in process_lines(lines= lines, pattern = patt) 
                    for pgr in program.aboves if program.aboves)
 
@@ -64,7 +65,40 @@ cntj (57)"""
 
 assert base(lines=TEST_INPUT, pattern=patt) =='tknk'
 
-with open("day07_input.txt", 'r') as file:
-    print(base(lines=file.read(), pattern=patt))
+# could not resolve Part 2 
+# example below copied from joel grus livecoding video
+# personal note recurssion is still a pain
+ 
 
+def balance_towers(programs_input: str):
+    lookups = { program.name: program for program in process_lines(programs_input, pattern=patt) }
+    
+    def check(program: Program):
+        """
+        Return the total weight and is_balanced
+        """
+        #print("checking", program)
+        subchecks = {name: check(lookups[name]) for name in program.aboves}
+        subcheck_weights = {weight for weight, _ in subchecks.values()}
+        is_balanced = len(subcheck_weights) <= 1
+        weight = program.weight + sum(weight for weight, _ in subchecks.values())
+        if len(subcheck_weights) > 1 and \
+            all(is_balanced for _, is_balanced in subchecks.values()):
+            print('program', program)
+            for name, (total_weight, _) in subchecks.items():
+                above_program = lookups[name]       
+                print(name, f'total weight is {total_weight}', above_program.weight)
+        #print(f'program is {program.name}', subchecks, subcheck_weights)
+        return weight, is_balanced 
+
+    root = lookups[base(programs_input, pattern=patt)]
+    check(root)
+
+#balance_towers(TEST_INPUT)
+
+if __name__ == "__main__":
+    with open("day07_input.txt", 'r') as file:
+        puzzle_input = file.read()
+        print(base(lines=puzzle_input, pattern=patt))
+        print(balance_towers(puzzle_input))    
 
